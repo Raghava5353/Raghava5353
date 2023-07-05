05e0b17b9e9899d2492d123e7318ccaba576ad78
Raghava <138653376+Raghava5353@users.noreply.github.com>
2023-07-05 13:36:03 +0530

    Add files via upload

---
+++ make your own mini dictionary.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+ascii_dict = {}
+
+for char in range(ord('a'), ord('z')+1):
+    ascii_dict[chr(char)] = char
+
+print("Dictionary:", ascii_dict)
+
+
+# In[ ]:
+
+
+
+
