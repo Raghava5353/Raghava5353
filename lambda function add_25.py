0123bb6bd5b3c8599160088d49987e2ef3bb197e
Raghava <138653376+Raghava5353@users.noreply.github.com>
2023-07-25 09:25:09 +0530

    Add files via upload

---
+++ lambda function add_25.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+add_25 = lambda x: x + 25
+
+# Test the lambda function
+sample_input = 10
+sample_output = add_25(sample_input)
+print(sample_output)  # Output will be 35
+
+
+# In[ ]:
+
+
+
+
